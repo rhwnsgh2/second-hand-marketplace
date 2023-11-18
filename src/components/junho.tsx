4e7ed7a4322638690187ffac1a0614f37e014c18
고준호 <76002467+rhwnsgh2@users.noreply.github.com>
2023-11-18 07:55:34 +0000

    준호컴포넌트생성

---
+++ src/components/junho.tsx
@@ -0,0 +1,3 @@
+export const Junho = () => {
+  return <div>안녕하세요 저는 준호입니다.</div>;
+};
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 export const Layout = () => {
   return (
     <div>
-      <div>이거 진짜 빠름</div>
+      <div>하이</div>
     </div>
   );
 };
