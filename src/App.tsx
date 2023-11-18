b2b4b2e83520019d945be3be8a9841714e433f42
junho <junho.ko@business-canvas.com>
2023-11-18 16:28:59 +0900

    add component folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import { Layout } from "./components/layout";
 
 function App() {
-  return <div>이거슨 중고거래 플랫폼</div>;
+  return <Layout />;
 }
 
 export default App;
---
+++ src/components/layout.tsx
@@ -0,0 +1,7 @@
+export const Layout = () => {
+  return (
+    <div>
+      <div>이거슨 중고거래 플랫폼</div>
+    </div>
+  );
+};
